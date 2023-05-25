3d1f79626c7ae211024ec98868fc67eb580493f3
inna9Z <vozniakinna@gmail.com>
2023-05-25 09:11:25 +0200

    added my exercises

--- lesson-plans/computers-and-developers/tracing-variables/write-your-examples/4-null.js
+++ lesson-plans/computers-and-developers/tracing-variables/write-your-examples/4-null.js
@@ -6,3 +6,15 @@ TODO: create a few variables(null):
 - you need to add comments to show where the variable been
 declared, initialized, read , reassigned,...
 */
+
+// Variable 1: firstName
+// Declared and initialization variable firstName
+let firstName = null;
+
+// Variable 2: age
+// Declared and initialization varianle age
+let age = null;
+
+// Reading the values of variables
+console.log(firstName, age)
+
